05ecb4dd91f43260914891de16d6bd17822e3d0e
paarthmadan <madanhockey@hotmail.com>
2016-10-22 19:09:46 -0400

    created basic window, constructor

--- src/TestGenerator.java
+++ src/TestGenerator.java
@@ -1,6 +1,27 @@
+import javax.swing.*;
 
 public class TestGenerator {
+
+	//CONSTANTS
+	final String APP_NAME = "Test Generator";
+	final int SCREEN_WIDTH = 500;
+	final int SCREEN_HEIGHT = 300;
+	
+	JFrame mainFrame;
+	
+	public TestGenerator(){
+		mainFrame = new JFrame();
+		
+		mainFrame.setTitle(APP_NAME);
+		mainFrame.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setLocationRelativeTo(null);
+		
+		mainFrame.setVisible(true);
+	}
+	
+	
 	public static void main(String[] args) {
-		//PROJECT SETUP
+		new TestGenerator();
 	}
 }
